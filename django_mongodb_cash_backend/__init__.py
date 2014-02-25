b7f50b0998f35a8e33d8c8432394af845c714608
Alireza Savand <alireza.savand@gmail.com>
2014-02-25 20:03:45 +0400

    Update copyright thing

--- django_mongodb_cash_backend/__init__.py
+++ django_mongodb_cash_backend/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# Author Karol Sikora <karol.sikora@laboratorium.ee>, (c) 2012
+# Author:
+#  Karol Sikora <karol.sikora@laboratorium.ee>, (c) 2012
+#  Alireza Savand <alireza.savand@gmail.com>, (c) 2013, 2014
 
 try:
     import cPickle as pickle
